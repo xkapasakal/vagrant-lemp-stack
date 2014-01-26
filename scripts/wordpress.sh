c8fa3200852aa7a6cf832283932524e1d7a38ac2
Christos Kapasakalidis <xkapasakal@gmail.com>
2014-01-27 00:59:11 +0200

    Remove readme.html

--- scripts/wordpress.sh
+++ scripts/wordpress.sh
@@ -4,6 +4,7 @@ tar -xzvf latest.tar.gz
 
 mysql -u root -ppassword < /vagrant/scripts/wordpress/config_mysql.sql
 
+sudo rm -f ~/wordpress/readme.html
 cp ~/wordpress/wp-config-sample.php ~/wordpress/wp-config.php
 sed -i "s/define('DB_NAME', '.*');/define('DB_NAME', 'wordpress');/g" ~/wordpress/wp-config.php
 sed -i "s/define('DB_USER', '.*');/define('DB_USER', 'wordpressuser');/g" ~/wordpress/wp-config.php
@@ -13,7 +14,3 @@ sudo cp -r ~/wordpress/* /var/www
 cd /var/www/
 sudo chown www-data:www-data * -R 
 sudo usermod -a -G www-data nginx
-
-cd ~
-sudo service nginx restart
-sudo service php5-fpm restart
